cc9d7584668d508cacb39e885fac01197cc09980
prudhvi3333 <90162931+prudhvi3333@users.noreply.github.com>
2021-12-24 15:42:22 +0530

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,24 @@
+pipeline{
+agent none
+stages
+{
+stage ('Build')
+{
+steps{
+echo "building the project..."
+}
+}
+stage ('Test')
+{
+steps{
+echo "testing the project..."
+}
+}
+stage ('Deploy')
+{
+steps{
+echo "deploying the project..."
+}
+}
+}
+}
